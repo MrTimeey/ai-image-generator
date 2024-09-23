7105123426ebdc6138d54283786824db1fc2e6da
Tim Kruse <coding@tim-kruse.com>
2024-09-23 16:21:51 +0200

    wip

--- src/common/timeUtils.ts
+++ src/common/timeUtils.ts
@@ -1,9 +1,14 @@
 import dayjs from 'dayjs';
+import timezone from 'dayjs/plugin/timezone';
+import utc from 'dayjs/plugin/utc';
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 export function currentTimestamp() {
     return formatDate(new Date());
 }
 
 const formatDate = (date: Date): string => {
-    return dayjs(date).format('YYYY-MM-DD_HH-mm');
+    return dayjs(date).tz('Europe/Berlin').format('YYYY-MM-DD_HH-mm');
 };
