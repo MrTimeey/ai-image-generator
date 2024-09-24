e5c260afc4c85df56bbc3f9ca6358d86651b37db
Tim Kruse <coding@mrtimeey.com>
2024-09-24 23:19:54 +0200

    add auth

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,55 @@ import * as path from 'path';
 import { cleanDataStore } from './common/dataStore';
 import thumbnails from './routes/thumbnails';
 import files from './routes/files';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+import jwt from 'jsonwebtoken';
 
 const app: express.Application = express();
 
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+const JWT_SECRET = 'meinSuperGeheimesSchluesselwort';
+
+// Benutzername und Passwort
+const username = 'admin';
+const password = 'password';
+
+// @ts-ignore
+const verifyToken = (req, res, next) => {
+    const token = req.cookies.token;
+    if (!token) {
+        return res.status(403).redirect('/login.html');
+    }
+    // @ts-ignore
+    jwt.verify(token, JWT_SECRET, (err, user) => {
+        if (err) {
+            return res.status(403).redirect('/login.html');
+        }
+        req.user = user;
+        next();
+    });
+};
+app.post('/login', (req, res) => {
+    const { username: inputUsername, password: inputPassword } = req.body;
+    if (inputUsername === username && inputPassword === password) {
+        const token = jwt.sign({ username: inputUsername }, JWT_SECRET, { expiresIn: '1h' });
+        res.cookie('token', token, { httpOnly: true, maxAge: 3600000 });
+        return res.status(200).send('Login erfolgreich');
+    }
+    return res.status(401).send('Ungültige Anmeldedaten');
+});
+
+app.use((req, res, next) => {
+    if (req.path === '/login' || req.path === '/login.html') {
+        return next();
+    }
+    verifyToken(req, res, next);
+});
+
 
 const apiRouter: express.Router = express.Router();
 apiRouter.use('/openai', openAi);
@@ -19,8 +63,6 @@ app.use('/api', apiRouter);
 app.use('/thumbnails', thumbnails);
 app.use('/files', files);
 app.get('/', (_, res) => res.redirect("/protected/index.html"));
-
-
 app.use(express.static(path.join(__dirname, 'static')));
 
 app.listen(config.port, () => {
