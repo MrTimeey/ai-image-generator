e99f43f611c510606ad5e865530ad9c6c56a6545
Tim Kruse <coding@mrtimeey.com>
2024-09-25 23:21:00 +0200

    Implemented new design

--- src/routes/thumbnails.ts
+++ src/routes/thumbnails.ts
@@ -12,60 +12,31 @@ const thumbnails: express.Router = express.Router();
 export const thumbnailDir = `${__dirname}/../static/thumbnails`;
 const imageDir = `${appConfig.baseFolder}`;
 
-thumbnails.get('/overview', async (req, res) => {
+export async function createThumbnail(image: string) {
+    const thumbnailPath = path.join(thumbnailDir, image);
+    if (!fs.existsSync(thumbnailPath)) {
+        await sharp(path.join(imageDir, image)).resize(200).toFile(thumbnailPath);
+    }
+}
+
+thumbnails.get('/all', async (req, res) => {
     fs.ensureDirSync(thumbnailDir);
-    fs.readdir(imageDir, async (err, files) => {
-        if (err) {
-            console.error(err);
-            return res.status(500).send('Fehler beim Laden der Bilder.');
-        }
+    const fileNames = fs.readdirSync(imageDir);
+    const images = fileNames.filter((file) => /\.(jpg|jpeg|png)$/.test(file));
 
-        const images = files.filter((file) => /\.(jpg|jpeg|png)$/.test(file));
+    for (const image of images) {
+        await createThumbnail(image);
+    }
+    const dataStore = getDataStore();
+    const imageMap: { [key: string]: DataImage } = dataStore.data.reduce((acc, i) => {
+        if (!i.fileName) return acc;
+        return { ...acc, [i.fileName]: i };
+    }, {})
+    images.sort(function(x, y) {
+        if (!imageMap[x]?.createdAt || !imageMap[y]?.createdAt) return -1;
+        return fromFormated(imageMap[x].createdAt).isBefore(fromFormated(imageMap[y].createdAt)) ? 0 : 1;
+    })
+    res.send(images)
+})
 
-        for (const image of images) {
-            const thumbnailPath = path.join(thumbnailDir, image);
-            if (!fs.existsSync(thumbnailPath)) {
-                await sharp(path.join(imageDir, image))
-                    .resize(200)
-                    .toFile(thumbnailPath);
-            }
-        }
-        const dataStore = getDataStore();
-        const imageMap: {[key: string]: DataImage} = dataStore.data.reduce((acc, i) => {
-            if (!i.fileName) return acc;
-            return {...acc, [i.fileName]: i};
-        }, {})
-        images.sort(function(x, y) {
-            if (!imageMap[x]?.createdAt || !imageMap[y]?.createdAt) return -1;
-            return fromFormated(imageMap[x].createdAt).isBefore(fromFormated(imageMap[y].createdAt)) ? 0 : 1;
-        })
-
-        const mapToImage = (image: string) => `
-                  <div style="display: inline-block; margin: 10px; width: 200px;">
-                    <a href="/files/open/${image}"><img src="/thumbnails/${image}" title="${image}" alt="${image}" style="max-height:200px; max-width: 200px" /></a>
-                  </div>
-                `;
-
-        const header = fs.readFileSync(`${__dirname}/../static/header.html`, 'utf-8');
-        res.send(`
-          <html>
-            <head>
-              <title>AI Image Generator</title>
-              <!-- Favicon -->
-              <link rel="apple-touch-icon" sizes="180x180" href="/favicon_io/apple-touch-icon.png" />
-              <link rel="icon" type="image/png" sizes="32x32" href="/favicon_io/favicon-32x32.png" />
-              <link rel="icon" type="image/png" sizes="16x16" href="/favicon_io/favicon-16x16.png" />
-              <link rel="manifest" href="/favicon_io/site.webmanifest" />
-              <link rel="stylesheet" href="/css/style.css" />
-            </head>
-            <body>
-              ${header}
-              <div>
-                ${images.map((image) => mapToImage(image)).join('')}
-              </div>
-            </body>
-          </html>
-        `);
-    });
-});
 export default thumbnails;
--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,30 +1,64 @@
-function onSubmit(e) {
-    e.preventDefault();
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+async function getDetailInformation(imageName) {
+    try {
+        const response = await fetch(`/api/files/get/${imageName}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
 
-    let msgElement = document.createElement('h2');
-    msgElement.classList.add('msg');
+        if (!response.ok) {
+            document.querySelector('#msg').textContent = 'That image could not be generated';
+            return {errorMsg: 'Image not found!'}
+        }
+        return await response.json();
+    } catch (error) {
+        document.querySelector('#msg').textContent = error;
+        return {errorMsg: 'Image not found!'}
+    }
+}
 
-    document.getElementsByClassName('image-container')[0].replaceChildren();
-    document.getElementsByClassName('image-container')[0].appendChild(msgElement);
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+async function getAllThumbnails() {
+    try {
+        const response = await fetch('/api/thumbnails/all', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
 
-    const prompt = document.querySelector('#prompt').value;
+        if (!response.ok) {
+            document.querySelector('#msg').textContent = 'That image could not be generated';
+            return []
+        }
+        return await response.json();
+    } catch (error) {
+        document.querySelector('#msg').textContent = error;
+        return [];
+    }
+}
+
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function generateImage() {
+    const prompt = document.querySelector('#description').value;
     const size = document.querySelector('#size').value;
     const quality = document.querySelector('#quality').value;
     const model = document.querySelector('#model').value;
     const amount = document.querySelector('#amount').value;
 
     if (prompt === '') {
-        alert('Please add some text');
-        return;
+        document.querySelector('#msg').textContent = 'Please add some text';
+        return [];
     }
 
-    generateImageRequest(prompt, model, size, amount, quality);
+    return generateImageRequest(prompt, model, size, amount, quality);
 }
 
 async function generateImageRequest(prompt, model, size, amount, quality) {
     try {
-        showSpinner();
-
         const response = await fetch('/api/openai/generate-images', {
             method: 'POST',
             headers: {
@@ -40,86 +74,26 @@ async function generateImageRequest(prompt, model, size, amount, quality) {
         });
 
         if (!response.ok) {
-            removeSpinner();
-            throw new Error('That image could not be generated');
+            document.querySelector('#msg').textContent = 'That image could not be generated';
+            return []
         }
-
         const data = await response.json();
-
-        let imagesContainer = document.getElementsByClassName('image-container')[0];
-        data.images.forEach((imageResponse) => {
-            let imageElement = document.createElement('img');
-            imageElement.src = imageResponse.url;
-            imagesContainer.appendChild(imageElement);
-        });
+        return data.images.map(i => i.fileName);
     } catch (error) {
-        document.querySelector('.msg').textContent = error;
-    } finally {
-        removeSpinner();
+        document.querySelector('#msg').textContent = error;
+        return [];
     }
 }
 
-function showSpinner() {
-    document.querySelector('.spinner').classList.add('show');
-}
 
-function removeSpinner() {
-    document.querySelector('.spinner').classList.remove('show');
-}
-
-function changePossibleValues() {
-    const selectedLanguageModel = languageModelValues.value;
-    if (selectedLanguageModel === 'DALL_E_TWO') {
-        addOptionValue('#quality', 'Standard', 'STANDARD', true);
-        removeOptionValue('#quality', 'HD');
-        addOptionValue('#amount', '1', '1', true);
-        addOptionValue('#amount', '2', '2');
-        addOptionValue('#amount', '3', '3');
-        addOptionValue('#amount', '4', '4');
-        addOptionValue('#size', 'Small', 'small', true);
-        addOptionValue('#size', 'Medium', 'medium');
-        addOptionValue('#size', 'Large', 'large');
-        removeOptionValue('#size', 'large_vertical');
-        removeOptionValue('#size', 'large_horizontal');
-    } else {
-        addOptionValue('#quality', 'Standard', 'STANDARD', true);
-        addOptionValue('#quality', 'HD', 'HD');
-        addOptionValue('#amount', '1', '1', true);
-        removeOptionValue('#amount', '2');
-        removeOptionValue('#amount', '3');
-        removeOptionValue('#amount', '4');
-        removeOptionValue('#size', 'small');
-        removeOptionValue('#size', 'medium');
-        addOptionValue('#size', 'Large', 'large', true);
-        addOptionValue('#size', 'Large Vertical', 'large_vertical');
-        addOptionValue('#size', 'Large Horizontal', 'large_horizontal');
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function showToast(message) {
+    const toast = document.getElementById("toast");
+    if (toast) {
+        toast.textContent = message;
+        toast.className = "toast show";
+        setTimeout(() => {
+            toast.className = toast.className.replace("show", "");
+        }, 3000);
     }
 }
-
-function addOptionValue(target, text, value, selected = false) {
-    for (const opt of document.querySelector(target).options) {
-        if (opt.value === value) {
-            if (selected) opt.selected = selected;
-            return;
-        }
-    }
-    const option = document.createElement('option');
-    option.value = value;
-    option.innerHTML = text;
-    option.selected = selected;
-
-    document.querySelector(target).appendChild(option);
-}
-
-function removeOptionValue(target, value) {
-    for (const opt of document.querySelector(target).options) {
-        if (opt.value === value) {
-            document.querySelector(target).removeChild(opt);
-        }
-    }
-}
-
-document.querySelector('#image-form').addEventListener('submit', onSubmit);
-
-const languageModelValues = document.querySelector('#model');
-languageModelValues.addEventListener('click', changePossibleValues);
