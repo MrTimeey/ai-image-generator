cb80343b22c2284db835ccc5659c195aa317a9e6
Tim Kruse <coding@mrtimeey.com>
2024-09-29 23:49:41 +0200

    wip

--- src/common/fileUtils.ts
+++ src/common/fileUtils.ts
@@ -2,16 +2,7 @@ import fs from 'fs';
 import { GeneratedImage, LanguageModel } from '../types';
 import { getDataStore, saveDataStore } from './dataStore';
 import appConfig from './appConfig';
-import { randomUUID } from 'crypto';
 import request from 'sync-request';
-
-const tempDir = './temp';
-
-export const createFolder = (name: string) => {
-    if (!fs.existsSync(name)) {
-        fs.mkdirSync(name);
-    }
-};
 
 export const getFileName = (id: string, createdAt: string): string => {
     return `${createdAt}_${id}.png`;
@@ -34,6 +25,7 @@ export const persistImage = (image: GeneratedImage, createdAt: string, languageM
 
 export function downloadFile(image: GeneratedImage): void {
     try {
+        console.log('Download start')
         const res = request('GET', image.url);
         if (res.statusCode === 200) {
             fs.writeFileSync(`${appConfig.baseFolder}/${image.fileName}`, res.getBody());
@@ -45,24 +37,3 @@ export function downloadFile(image: GeneratedImage): void {
         console.error(`Error downloading file: ${error.message}`);
     }
 }
-
-export const createTempImage = (base64String: string): string => {
-    createFolder('./temp');
-    const regex = /data:image\/(.*?);base64,/;
-    let fileEnding = 'png';
-    if (regex.test(base64String)) {
-        const matchedString = base64String.match(regex) ?? [];
-        fileEnding = matchedString.length > 0 ? matchedString[1] : 'png';
-        base64String = base64String.replace(regex, '');
-    }
-    const buffer = Buffer.from(base64String, 'base64');
-    const targetPath = `${tempDir}/${randomUUID()}.${fileEnding}`;
-    fs.writeFileSync(targetPath, buffer);
-    return targetPath;
-};
-
-export const deleteTempFolder = () => {
-    if (fs.existsSync(tempDir)) {
-        fs.rmSync(tempDir, { recursive: true, force: true });
-    }
-};
--- src/routes/openAi.ts
+++ src/routes/openAi.ts
@@ -52,8 +52,11 @@ openAi.post('/generate-images', async (req, res) => {
     }
     if (appConfig.saveImagesEnabled) {
         for (const image of images.images) {
+            console.log(1)
             persistImage(image, images.createdAt, images.languageModel, images.description);
+            console.log(2)
             downloadFile(image);
+            console.log(3)
             await createBigThumbnail(image.fileName);
             await createThumbnail(image.fileName)
         }
